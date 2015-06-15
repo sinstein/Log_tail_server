6db13758a62785ecfe3b91e5c25c1efb25098897
sinstein <sauravkothari@Sauravs-MacBook-Pro.local>
2015-06-15 21:25:07 +0530

    Fixes and improvements

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+/*
+The followinfg modules installed and included
+  # byline - to read stream line by line
+  # socket.io - to create socket for communication between server and client
+--
+os.EOL - to add platform specific end of line symbol
+--
+*/
+
 var fs = require('fs'),
   http = require('http'),
   io = require('socket.io'),
-  lineReader = require('line-reader');
+  endOfLine = require('os').EOL,
+  byLine = require('byline');
 
 var port = process.argv[3];
 var file = process.argv[2];
-var oldLogs = fs.readFileSync(file, 'utf8');
 
+//Basic check to ensure required number of arguments are entered
 if (process.argv.length < 4) {
   console.log("Usage: node index.js [logFile] [port]");
   process.exit(-1);
@@ -23,44 +33,40 @@ var server = http.createServer(function(req, res) {
 
 var socket = io.listen(server);
 var continueFrom = 0;
-var arr = new Array();
+var tail = new Array();
+var inStream = byLine(fs.createReadStream(file, {start: continueFrom }));
 
-lineReader.eachLine(file,function(line, last) {
-    continueFrom += line.length;
-    if(arr.length < 10) {
-        arr.push(line);
-    }
-    else {
-        arr.shift();
-        arr.push(line);
-    }
+var addToTail = function (line) {
+  line += endOfLine;
+  continueFrom += line.length;
+  tail.push(line);
+
+  if (tail.length > 10) {
+    tail.shift();
+  }
+}
+
+//Intial tail. To be displayed when user connects
+inStream.on('data', function(line) {
+  addToTail(line.toString());
 });
 
+
+//Watches over log file and responds to changes
 fs.watch(file, function(event, fname) {
   if (event == 'change') {
-    var readStream = fs.createReadStream(file, {
-      start: (continueFrom - 1)
-    });
-    var text = '';
-    readStream.on('data', function(chunk) {
-
-      text += chunk.toString();
-    });
-    readStream.on('end', function() {
-	   continueFrom += text.length
-      socket.emit('message',text);
+    var readStream = byLine(fs.createReadStream(file, {start: continueFrom }));
+    readStream.on('data', function(line) {
+      addToTail(line.toString());
+      line += endOfLine;
+      socket.emit('message',line.toString())
     });
   }
 });
 
-var text = "";
+
 socket.on('connection', function(socket) {
-  emitPrevious(arr, socket)
-
-});
-
-var emitPrevious = function(logArr, socket) {
-  logArr.forEach(function(log) {
-  	socket.emit('message',log);
+  tail.forEach(function (log){
+    socket.emit('message',log);
   });
-}
+});
